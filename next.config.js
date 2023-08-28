bd771da718d98fda4f8f9a5d2e044038ed0ec873
Shobhit <Shobhitjaguri21751@gmail.com>
2023-08-28 20:46:44 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['techcrunch.com'],
+    domains: ['static.toiimg.com'],
+
+    domains: ['c.ndtvimg.com', /* other domains */],
+  },
+
 }
 
 module.exports = nextConfig
